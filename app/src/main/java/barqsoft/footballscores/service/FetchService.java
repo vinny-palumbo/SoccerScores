95c62f56764e18b5452a5cf94e147b25987777f6
Vinny <vin_palumbo@hotmail.com>
2016-02-24 14:06:00 -0500

    Refactor rename FetchService class

--- app/src/main/java/barqsoft/footballscores/service/FetchService.java
+++ app/src/main/java/barqsoft/footballscores/service/FetchService.java
@@ -28,12 +28,13 @@ import barqsoft.footballscores.R;
 /**
  * Created by yehya khaled on 3/2/2015.
  */
-public class myFetchService extends IntentService
+public class FetchService extends IntentService
 {
-    public static final String LOG_TAG = "myFetchService";
-    public myFetchService()
+    public static final String LOG_TAG = "FetchService";
+
+    public FetchService()
     {
-        super("myFetchService");
+        super("FetchService");
     }
 
     @Override
